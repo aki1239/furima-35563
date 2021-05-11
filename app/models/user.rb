7bcceb526147c2a90f46669b3aa3661c54fee285
aki1239 <82556115+aki1239@users.noreply.github.com>
2021-05-11 12:19:36 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+        has_many :items
+         has_many :histories
+        #with_options presence: true do
+        validates :nickname,presence: true   #format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+        validates :family_name, presence: true
+        validates :first_name, presence: true
+        validates :family_name_kana, presence: true
+        validates :first_name_kana, presence: true
+        validates :birth_date,presence: true
 end
