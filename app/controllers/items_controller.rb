38bea60f31c1b60831434610f56f479228f1ed70
aki1239 <82556115+aki1239@users.noreply.github.com>
2021-05-12 16:31:13 +0900

    activestrageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+ private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:price,:description,:category_id,:status_id,:cost_id,:prefecture_id,:days_id,:user_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
 
   belongs_to :user_id
+  has_one_attached :image
   with_options presence: true do
     validates :name
     validates :price
