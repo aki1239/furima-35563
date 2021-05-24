f2fcdf174cedd71263d7e22881830fb59aceeb68
aki1239 <82556115+aki1239@users.noreply.github.com>
2021-05-24 15:46:21 +0900

    出品

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :name, length: { maximum: 40 }
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 9_999_999 }, format: { with: /\A^[0-9]*$\z/ }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000 }, format: { with: /\A^[0-9]*$\z/ }
     # validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :description, length: { maximum: 1000 }
     validates :image
